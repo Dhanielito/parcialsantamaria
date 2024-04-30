e902c54f8e340bf54a78d968ee59facca7d8ddf2
Dhanielito <dhanielsantamaria@gmail.com>
2024-04-29 19:53:45 -0500

    Primer commit: Código inicial de mi proyecto de Laravel

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('index');
+    }
+
+    public function register()
+    {
+        return view('registrarse');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function store(Request $request){
+        $validate = $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required',
+            'confirm-password' => 'required|same:password'
+        ]);
+        $data = $request->except('confirm-password', 'password');
+        $data['password'] = Hash::make($request->password);
+        User::create($data);
+        return redirect('/login');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
+
+//Santamaria Jose Dhaniel - este
